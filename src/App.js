43ca32deef19eec6be115eab907184505d96f780
faical-allou <faical.allou@gmail.com>
2019-07-14 15:53:01 +0200

    added navigation between screens

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View, Linking, StyleSheet  } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, Linking, StyleSheet, Button } from 'react-native';
+import { createStackNavigator, createAppContainer,StackActions, NavigationActions } from "react-navigation";
 
-export default class App extends React.Component {
+
+class HomeScreen extends React.Component {
 
   constructor(props){
     super(props);
@@ -43,9 +45,63 @@ export default class App extends React.Component {
         <FlatList
           data={this.state.dataSource}
           renderItem={({item}) => <View style={{height:150,backgroundColor:"red",padding:60,margin:20,fontSize:32, alignItems:"center"}  } >
-            <Text style={styles.One} onPress={ ()=> Linking.openURL('https://google.com/search?q='+item.name) }>{item.name}</Text>
+            <Text style={styles.One} onPress={() => this.props.navigation.navigate('Details')}>{item.name}</Text>
             </View>}
           keyExtractor={({id}, index) => id.toString()}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://flocals.herokuapp.com/home/Food') 
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20, justifyContent:"center", backgroundColor:"#fff", alignItems:"center"}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <View style={{height:150,backgroundColor:"red",padding:60,margin:20,fontSize:32, alignItems:"center"}  } >
+            <Text style={styles.One} onPress={ ()=> Linking.openURL('https://google.com/search?q='+item.name) }>{item.name},{item.loc_short}, </Text>
+            </View>}
+          keyExtractor={({activity_id}, index) => activity_id.toString()}
+        />
+        <Button
+          title="Go back"
+          onPress={() => this.props.navigation.goBack()}
         />
       </View>
     );
@@ -65,4 +121,17 @@ const styles = StyleSheet.create({
     // Define font size here in Pixels
     fontSize : 30
   }
+})
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+}, {
+    initialRouteName: 'Home',
 });
+
+export default createAppContainer(AppNavigator);
