0949709b09b7dabc32115e9571f9ebddb99ae29f
faical-allou <faical.allou@gmail.com>
2019-07-17 23:32:44 +0200

    login with google works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View, Linking, StyleSheet, Button } from 'react-native';
+import { FlatList, ActivityIndicator, Text, View, Linking, StyleSheet, Button, Image } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import { GiftedChat } from 'react-native-gifted-chat'
+import { Google } from 'expo';
 
 import helper from '../utils/helper.js';
+
 
 
 class HomeScreen extends React.Component {
@@ -38,8 +40,31 @@ class HomeScreen extends React.Component {
 class DetailsScreen extends React.Component {
   constructor(props){
     super(props);
-    this.state ={ isLoading: true};    
+    this.state ={ isLoading: true,username:'', photoUrl:'', signedIn:false };    
   }
+
+  signIn = async () => {
+    try {
+      const result = await Google.logInAsync({
+        expoClientId: '746916049107-0un5svk32nv9o90c6vccek36tcfsiud0.apps.googleusercontent.com',
+        iosClientId: '746916049107-6p47litmq08pgf8bbbmfh00vjt79qct1.apps.googleusercontent.com',
+        scopes: ["profile", "email"]
+      })
+
+      if (result.type === "success") {
+        this.setState({
+          signedIn: true,
+          username: result.user.name,
+          photoUrl: result.user.photoUrl
+        })
+      } else {
+        console.log("cancelled")
+      }
+    } catch (e) {
+      console.log("error", e)
+    }
+  }
+
   componentDidMount(){
     const { navigation } = this.props;
     const current_location = navigation.getParam('destination', '');
@@ -55,18 +80,28 @@ class DetailsScreen extends React.Component {
       <View style={styles.listElements}>
         <FlatList
           data={this.state.dataSource}
-          renderItem={({item}) => <View><View style={styles.itemElement} >
+          renderItem={({item}) => <View style={styles.itemElement} >
               <Text style={styles.textElement} onPress={ ()=> Linking.openURL('https://google.com/search?q='+item.name) }>{item.name}</Text>
               <Text style={styles.textElement} onPress={ ()=> this.props.navigation.navigate('Chat', {destination: 'home/'+item.city})}>{item.city}</Text>
               <Text style={styles.textElement}> {item.loc_short}</Text>
               </View>
-              <View style={styles.itemElements}>
-                <Text style={styles.textElement} onPress={ ()=> helper.postForm(this, variables.endpoint+'/api/v1/home/newactivity', this.state.dataSource[0])}> ... Add Stuff ..</Text>
-              </View>
-            </View>
+            
             }
           keyExtractor={({activity_id}, index) => activity_id.toString()}
         />
+
+        <View style={styles.container}>
+        {this.state.signedIn ? (
+          <View>
+          <LoggedinBar username={this.state.username} photoUrl={this.state.photoUrl} />
+          <Button
+          title="Add Stuff"
+          onPress={() => helper.postForm(this, variables.endpoint+'/api/v1/home/newactivity',this.dataSource[0])}
+          /></View>
+        ) : (
+          <LoginBar signIn={this.signIn} />
+        )}
+      </View>
         <Button
           title="Go back"
           onPress={() => this.props.navigation.goBack()}
@@ -118,6 +153,22 @@ class Chatscreen extends React.Component {
 }
 
 
+const LoginBar = props => {
+  return (
+    <View>
+      <Button title="Sign in with Google to add stuff " onPress={() => props.signIn()} />
+    </View>
+  )
+}
+
+const LoggedinBar = props => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Welcome: {props.username}</Text>
+      <Image style={styles.image} source={{ uri: props.photoUrl }} />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   itemElement: {
@@ -147,7 +198,7 @@ const styles = StyleSheet.create({
 const AppNavigator = createStackNavigator({
   Home: {screen: HomeScreen},
   Details: {screen: DetailsScreen},
-  Chat: {screen: Chatscreen}, 
+  Chat: {screen: Chatscreen}
 }, 
 { initialRouteName: 'Home'}
 );
