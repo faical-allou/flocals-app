814ef9863f2d6c2547663e06049f7686286002a3
faical-allou <faical.allou@gmail.com>
2019-07-20 23:45:39 +0200

    added dictionnary to match our classification with big G

---
+++ utils/dict.js
@@ -0,0 +1,28 @@
+
+const dict = {
+    us2g:{
+    default: ['point_of_interest', 'establishment'],
+    food: ['food', 'restaurant'],
+    shopping: ['supermarket', 'shopping_mall', 'store'],
+    nature: ['natural_feature'],
+    nightlife: ['bar', 'nightclub'],
+    park: ['park'],
+    artnhistory: ['museum', 'art_gallery']
+},
+    g2us: {
+        point_of_interest: 'default',
+        establishment: 'default',
+        museum: 'artnhistory',
+        shopping_mall: 'shopping',
+        park: 'park',
+        supermarket: 'shopping',
+        grocery_or_supermarket: 'shopping',
+        food: 'food',
+        store: 'shopping',
+        natural_feature: 'nature',
+        bar: 'nightlife',
+        restaurant: 'food',
+        night_club: 'nightlife'
+    }
+}
+export default dict;
